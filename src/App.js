1c11e01e0b486d1789dfa512d4cc2e39da5cfe4d
AntonZelk <zelkdar@gmail.com>
2021-05-25 21:47:52 +0300

    add esLint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, {Component} from 'react'
-
+import React, { Component } from 'react';
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Hello React</h1>
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
